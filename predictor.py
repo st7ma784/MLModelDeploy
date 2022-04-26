ddc58e629f513dff7bab1ecde51cff98f6dd8b44
Stephen <37083603+st7ma784@users.noreply.github.com>
2022-04-26 11:49:14 +0100

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -1,19 +1,10 @@
 
-from DataModule import Preprocessor
+from DataModule import Preprocessor,GLUEDataModule
 from model.py import GLUETransformer
 class Predictor(BasePredictor):
-      glue_task_num_labels = {
-        "cola": 2,
-        "sst2": 2,
-        "mrpc": 2,
-        "qqp": 2,
-        "stsb": 1,
-        "mnli": 3,
-        "qnli": 2,
-        "rte": 2,
-        "wnli": 2,
-        "ax": 3,
-    }
+    glue_task_num_labels = GLUEDataModule.glue_task_num_labels
+    
+    task_text_field_map = GLUEDataModule.task_text_field_map
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
         model_name_or_path="albert-base-v2"
@@ -23,7 +14,10 @@ class Predictor(BasePredictor):
             num_labels=self.glue_task_num_labels[task_name],
             task_name=task_name,
         )
-        self.preprocessor=Preprocessor(model_name_or_path)
+        self.preprocessor=Preprocessor(model_name_or_path,
+                                       text_fields=self.task_text_field_map[task_name],
+                                       max_seq_length=164
+                                      )
     
     # The arguments and types the model takes as input
     
