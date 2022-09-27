e46c910e3447f8f2761c6a359100135d3511ef9d
Stephen <37083603+st7ma784@users.noreply.github.com>
2022-09-27 11:01:38 +0100

    Update SLURM-Train.py

--- SLURM-Train.py
+++ SLURM-Train.py
@@ -1,28 +1,34 @@
-from test_tube.hpc import SlurmCluster
-from demoTrainScript import train
-from demoParser import parser
+
+from test_tube import SlurmCluster
+from trainclip_v2 import wandbtrain as train_clip   ###########<<<<<< INSERT YOUR TRAIN FuNCITON HERE!
+
+
+from HOparser import parser
 if __name__ == '__main__':
-   
+    from functools import partial
+    train=partial(train_clip,dir="/nobackup/projects/bdlan05/$USER/data/")     ###<<<<<<  RUNS YOUR TRAIN Function with CUSTOM DATA DIR 
+
+
     argsparser = parser(strategy='random_search')
     hyperparams = argsparser.parse_args()
 
     # Enable cluster training.
     cluster = SlurmCluster(
         hyperparam_optimizer=hyperparams,
-        log_path=hyperparams.log_path,
+        log_path="/nobackup/projects/bdlan05/$USER/logs/",#hyperparams.log_path,
         python_cmd='python3',
-        test_tube_exp_name=hyperparams.test_tube_exp_name
+#        test_tube_exp_name="PL_test"
     )
 
     # Email results if your hpc supports it.
     cluster.notify_job_status(
-        email='some@email.com', on_done=True, on_fail=True)
+        email='ExampleUser@Rando-Email.com', on_done=True, on_fail=True)           #<<<<<<<<  PUT YOUR EMAIL HERE
 
     # SLURM Module to load.
-    cluster.load_modules([
-        'python-3',
-        'anaconda3'
-    ])
+    # cluster.load_modules([
+    #     'python-3',
+    #     'anaconda3'
+    # ])
 
     # Add commands to the non-SLURM portion.
     
@@ -32,16 +38,25 @@ if __name__ == '__main__':
     # #comment
     # #SBATCH --cmd=value
     # ############
-    # cluster.add_slurm_cmd(
-    #    cmd='cpus-per-task', value='1', comment='CPUS per task.')
+    cluster.add_slurm_cmd(
+        cmd='account', value='bdlan05', comment='Project account for Bede')    #<<<<<EDIT TO YOUR BILLING ACCOUNT
+    cluster.add_slurm_cmd(
+        cmd='partition', value='gpu', comment='request gpu partition on Bede')   
 
     # Set job compute details (this will apply PER set of hyperparameters.)
-    cluster.per_experiment_nb_gpus = 4
-    cluster.per_experiment_nb_nodes = 2
+    #print(cluster.__dir__())
+    #del cluster.memory_mb_per_node
+    #This is commented because on bede, having gone into 
+    #nano /nobackup/projects/bdlan05/smander3/miniconda/envs/open-ce/lib/python3.9/site-packages/test_tube/hpc.py
+    #and removed memory per node and adjusted to not include cpu counts as this is done automatically in bede 
+    #del cluster.per_experiment_nb_cpus
+    cluster.cpus_per_task=0
+    cluster.per_experiment_nb_gpus = 4     #<<<request multiple GPUS
+    cluster.per_experiment_nb_nodes = 2    # ON HOW MANY NODES?
     #cluster.gpu_type = '1080ti'
 
-    # we'll request 40GB of memory per node
-    cluster.memory_mb_per_node = 40000
+    # we'll request 100GB of memory per node
+    #cluster.memory_mb_per_node = 0
 
     # set a walltime of 24 hours,0, minues
     cluster.job_time = '24:00:00'
@@ -49,6 +64,6 @@ if __name__ == '__main__':
     # 1 minute before walltime is up, SlurmCluster will launch a continuation job and kill this job.
     # you must provide your own loading and saving function which the cluster object will call
     cluster.minutes_to_checkpoint_before_walltime = 1
-
+    print(cluster.__dir__())
     # run the models on the cluster
-    cluster.optimize_parallel_cluster_gpu(train, nb_trials=20, job_name='first_tt_batch', job_display_name='my_batch') # Change this to optimize_parralel_cluster_cpu to debug.
+    cluster.optimize_parallel_cluster_gpu(train, nb_trials=2, job_name='fourth_wandb_trial_batch') # Change this to optimize_parralel_cluster_cpu to debug.
